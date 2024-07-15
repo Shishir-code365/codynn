cfebac1716673954db7a0ed3f36adcc006af1fcc
Shishir-code365 <dhakal.shishir2059@gmail.com>
2024-07-15 14:28:41 +0545

    repo done

--- codynn-api/controllers/repository.controller.js
+++ codynn-api/controllers/repository.controller.js
@@ -1,5 +1,5 @@
 const repoModel = require('../models/repository.schema');
-
+const languageModel = require('../models/language.schema');
 const createRepository = async (req, res) => {
     try {
         const MAX_REPOSITORIES_PER_PAGE = parseInt(req.query.limit);
@@ -27,6 +27,30 @@ const createRepository = async (req, res) => {
 const getRepositories = async(req, res) => {
     try{
 
+        const repoID = req.params.id;
+        if(repoID)
+        {
+            const repository = await repoModel.findById(repoID).populate('language');
+            if (!repository) {
+                return res.status(404).json({ error: 'Repository not found' });
+            }
+            return res.status(200).json({ repository });
+        }
+        const MAX_REPOSITORIES_PER_PAGE = parseInt(req.query.limit);
+        const { startIndex, limit } = req.pagination;
+        const repositories = await repoModel.find()
+            .populate('language')
+            .limit(limit)
+            .skip(startIndex);
+        const totalRepositories = await repoModel.countDocuments();
+        const totalPages = Math.ceil(totalRepositories / MAX_REPOSITORIES_PER_PAGE);
+        res.status(200).json({
+            total: totalRepositories,
+            page: req.pagination.page,
+            limit: req.pagination.limit,
+            totalPages: totalPages,
+            repositories
+        });
     }
     catch(error){
         console.log(error);
@@ -34,4 +58,56 @@ const getRepositories = async(req, res) => {
     }
 }
 
-module.exports = { createRepository,getRepositories};
+const upateRepository = async (req, res) => {
+    try {
+        const repoID = req.params.id;
+        const { title, language, noOfLessons } = req.body;
+        const repository = await repoModel.findByIdAndUpdate(repoID, {
+            title,
+            language,
+            noOfLessons
+        }, { new: true });
+
+        const validLanguage = await languageModel.findById(language);
+        if(!validLanguage){
+            return res.status(404).json({ error: 'Language not found' });
+        }
+        if (!repository) {
+            return res.status(404).json({ error: 'Repository not found' });
+        }
+        res.status(200).json({ message: 'Repository updated successfully', repository });
+    } catch (error) {
+        console.error('Error updating repository:', error);
+        res.status(500).json({ error: 'Server error' });
+    }
+}
+
+const deleteRepository = async (req, res) => {
+    try {
+        const repoID = req.params.id;
+        const deletedRepository = await repoModel.findByIdAndDelete(repoID);
+        if (!deletedRepository) {
+            return res.status(404).json({ error: 'Repository not found' });
+        }
+        res.status(200).json({ message: 'Repository deleted successfully', deletedRepository });
+    } catch (error) {
+        console.error('Error deleting repository:', error);
+        res.status(500).json({ error: 'Server error' });
+    }
+}
+
+const searchRepo = async(req,res)=>{
+    try{
+
+        const repository = await repoModel.find({ title: { $regex: req.body.search, $options: 'i' } });
+        res.status(200).json({ repository });
+
+    }
+    catch(error)
+    {
+        console.log(error);
+        return res.status(404).json({ error: 'Server error' });
+    }
+}
+
+module.exports = { createRepository,getRepositories,upateRepository,deleteRepository,searchRepo };
--- codynn-api/routes/repository.routes.js
+++ codynn-api/routes/repository.routes.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-const {createRepository} = require('../controllers/repository.controller');
+const {createRepository,getRepositories,deleteRepository,upateRepository,searchRepo} = require('../controllers/repository.controller');
 const paginationMiddleware = require('../middleware/pagination.middleware');
 
 router.post('/create',paginationMiddleware(), createRepository);
+router.get('/get/:id?', paginationMiddleware(), getRepositories);
+router.put('/update/:id', upateRepository);
+router.delete('/delete/:id', deleteRepository);
+router.get('/search', searchRepo);
 
 
 module.exports = router;
